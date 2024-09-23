1eada42af0b5e6c9d5e3f87203a11d3952177fda
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2024-09-23 23:22:20 +0200

    -EviL lintian

--- run_salsa.sh
+++ run_salsa.sh
@@ -4,4 +4,4 @@ set -e
 
 cd  ~/code
 docker run -v /home/jlblanco/code/mrpt-salsa:/tmp/mrpt-salsa -it --rm mrpt_builder \
-    /bin/bash -c "cd /tmp && mkdir build && cp -r mrpt-salsa build/ && cd build/mrpt-salsa && rm .vscode/ -fr && gbp buildpackage && bash"
+    /bin/bash -c "cd /tmp && mkdir build && cp -r mrpt-salsa build/ && cd build/mrpt-salsa && rm .vscode/ -fr && gbp buildpackage --git-builder='dpkg-buildpackage -S -us -uc' && lintian -EvIL +pedantic -i ../*.dsc && gbp buildpackage && lintian -EvIL +pedantic -i ../*.deb"
